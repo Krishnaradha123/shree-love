8a0d36a3738c2fd5e400f6947a40620f95b9a97d
Malaya143 <pedamalaya@gmail.com>
2024-10-30 13:46:00 +0530

    first commit

---
+++ firstPage.js
@@ -0,0 +1,31 @@
+
+const img = document.getElementById('image');
+
+const images = {
+    hi: "Pages/Assets/hi.gif",
+    ori: "Pages/Assets/sad.gif"
+}
+
+function ChangeImage(giphy) {
+    img.src = images[giphy];
+}
+function ResetImage() {
+    img.src = images.ori;
+};
+
+
+//Yes button bg image
+var body = document.body;
+
+function bg() {
+	body.className = 'hovered';
+};
+
+function resetBG(){
+	body.className = '';
+};
+
+//Linking to second page
+function myFunction() {
+    window.location.href = "Pages/Page 1/secondPage.html";
+}
